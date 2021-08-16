033ad6ed4f943f0a64b119e24b261e5c08403500
furkanaliunal <35196666+furkanaliunal@users.noreply.github.com>
2021-08-16 18:11:00 +0300

    Default flag is changed to aikar's

--- osserverlauncher.bat
+++ osserverlauncher.bat
@@ -19,7 +19,7 @@ title Onwexry's Server Console Kurulum 2/3
 echo Ramini Mb Cinsinden Gir
 set /p ram=
 set ram > ossettings.ini
-set flags= -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dnashorn.args=--no-deprecation-warning -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true
+set flags= -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Duser.language=EN -Dnashorn.args=--no-deprecation-warning -Daikars.new.flags=true
 set title >> ossettings.ini
 set flags >> ossettings.ini
 cls
